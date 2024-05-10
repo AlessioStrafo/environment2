a8d758324c73feefe1f790be68c5e984b150eaac
AlessioStrafo <straforini.alessio@gmail.com>
2024-05-10 12:42:59 +0200

    Es in corso

---
+++ src/test/java/co/develhope/environment2/Environment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.environment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Environment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
